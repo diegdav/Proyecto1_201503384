615ab1ca3cd68d0d3b3c910099755fac05c82f79
diego <diego@DESKTOP-J4RC1E8>
2018-03-10 15:07:47 -0600

    Proyecto1 bibliografias

---
+++ src/ipc1/proyecto1_201503384/Bibliografias.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.proyecto1_201503384;
+
+/**
+ *
+ * @author diego
+ */
+public class Bibliografias {
+    
+    private String autor;
+    private int anio_publicacion;
+    private String titulo;
+    private int edicion;
+    
+    
+}
