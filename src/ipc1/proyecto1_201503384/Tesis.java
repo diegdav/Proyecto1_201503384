05b7625212a5fd0d9da83904ce4751d7cb9b7a67
diego <diego@DESKTOP-J4RC1E8>
2018-03-10 20:05:51 -0600

    Proyecto1 tesis

---
+++ src/ipc1/proyecto1_201503384/Tesis.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.proyecto1_201503384;
+
+/**
+ *
+ * @author diego
+ */
+public class Tesis extends Bibliografia {
+    
+    private String area;
+    private int copias;
+    private int disponibles;
+
+    /**
+     * @return the area
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    /**
+     * @return the copias
+     */
+    public int getCopias() {
+        return copias;
+    }
+
+    /**
+     * @param copias the copias to set
+     */
+    public void setCopias(int copias) {
+        this.copias = copias;
+    }
+
+    /**
+     * @return the disponibles
+     */
+    public int getDisponibles() {
+        return disponibles;
+    }
+
+    /**
+     * @param disponibles the disponibles to set
+     */
+    public void setDisponibles(int disponibles) {
+        this.disponibles = disponibles;
+    }
+    
+}
