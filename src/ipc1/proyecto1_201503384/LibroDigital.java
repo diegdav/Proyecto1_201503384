f178505ae188ddd5f4d68131a31176c72872c290
diego <diego@DESKTOP-J4RC1E8>
2018-03-10 20:06:03 -0600

    Proyecto1 LibroDigital

---
+++ src/ipc1/proyecto1_201503384/LibroDigital.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.proyecto1_201503384;
+
+/**
+ *
+ * @author diego
+ */
+public class LibroDigital extends Bibliografia {
+    
+    private int tamanio;
+
+    /**
+     * @return the tamanio
+     */
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    /**
+     * @param tamanio the tamanio to set
+     */
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+    
+}
